943dc3accb37a3ee8ea3f65dc309109805ab90e5
RASIKA SHINDE <rasika.shinde@invenio-solutions.com>
2019-11-01 10:23:40 +0000

    change 1
    Change-Id: I73c24a3a41157736459972b44152f23a5e80b30c

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("selFragment.selectDialog.controller.View1", {
+		onValuehelp : function (oEvent) {
+			var id = oEvent.getParameters().id.split('--')[2];
+			var oDialog=  new sap.ui.xmlfragment("selFragment.selectDialog.fragments.selectDialog",this);
+			this.getView().addDependent(oDialog);
+			if(id == "input1"){
+				oDialog.bindAggregation("items",{
+				path : "city>/city",
+				template : new sap.m.StandardListItem({
+					title : "{city>cityName}"
+				})
+			});	
+			}else
+			{
+			 oDialog.bindAggregation("items",{
+				path : "country>/Country",
+				template : new sap.m.StandardListItem({
+					title : "{country>countryName}"
+				})
+			});	
+			}
+			
+			
+			oDialog.open();
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"selFragment/selectDialog/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
